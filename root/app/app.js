3bad65f6db61c87aaa0cca3c3fade2b98b5b3994
Dylan Smith <dylan_smith@apple.com>
2014-06-26 17:59:05 +0100

    improved app test coverage

--- root/app/app.js
+++ root/app/app.js
@@ -9,6 +9,7 @@ function(core, Router, HomeView) {
 
     var App = {
 
+        initialised: false,
         config: core.config,
         currentView: null,
         router: new Router(),
@@ -21,54 +22,25 @@ function(core, Router, HomeView) {
                 this.config.setenv(env);
             }
 
-            // listen for config changes
-            this.config.on('config:update', this.configObserver, this);
+            if (this.initialised === false) {
+                // listen for config changes
+                this.config.on('config:update', this.configObserver, this);
 
-            // respond to routes
-            this.router.on('route:home', _.bind(this.setView, this, 'home'));
-            this.router.on('route:setView', _.bind(this.setView, this));
-            this.router.on('route:setTheme', _.bind(this.setTheme, this));
+                // respond to routes
+                this.router.on('route:home', _.bind(this.setView, this, 'home'));
+                this.router.on('route:setView', this.setView, this);
+                this.router.on('route:setTheme', this.setTheme, this);
+            }
 
             // set the default view
             this.currentView = new HomeView();
 
             // commence routing
-            core.Backbone.history.start();
-        },
-
-        render: function() {
-            this.currentView.render();
-        },
-
-        setView: function(viewId) {
-            var app = this;
-            try {
-                require(
-                    ['views/' + viewId],
-                    function(View) {
-                        app.currentView = new View();
-                        app.currentView.render();
-                    },
-                    function(err) {
-                        app.setView('404');
-                    },
-                    undefined,
-                    // force syncronous require for prod environment running almond,
-                    // otherwise we lose the callstack for try/catch due to setTimeout
-                    (this.config.getenv() === 'prod')
-                );
+            if (!core.Backbone.History.started) {
+                core.Backbone.history.start();
             }
-            catch (err) {
-                // prevent infinite recursion
-                if (viewId !== '404') {
-                    app.setView('404');
-                }
-            }
-        },
 
-        setTheme: function(themeId) {
-            this.config.set('theme', themeId);
-            this.render();
+            this.initialised = true;
         },
 
         configObserver: function(evt) {
@@ -82,6 +54,47 @@ function(core, Router, HomeView) {
                     $('body').attr('data-' + evt.key, evt.newval);
                 }
             }
+        },
+
+        render: function() {
+            this.currentView.render();
+        },
+
+        setView: function(viewId, callback, errback) {
+            var app = this;
+            try {
+                require(
+                    ['views/' + viewId],
+                    function(View) {
+                        app.currentView = new View();
+                        app.currentView.render();
+                        if (_.isFunction(callback)) {
+                            callback(app.currentView);
+                        }
+                    },
+                    function(err) {
+                        app.setView('404', callback, errback);
+                    },
+                    undefined,
+                    // force syncronous require for prod environment running almond,
+                    // otherwise we lose the callstack for try/catch due to setTimeout
+                    (this.config.getenv() === 'prod')
+                );
+            }
+            catch (err) {
+                // prevent infinite recursion
+                if (viewId !== '404') {
+                    app.setView('404');
+                }
+                if (_.isFunction(errback)) {
+                    errback();
+                }
+            }
+        },
+
+        setTheme: function(themeId) {
+            this.config.set('theme', themeId);
+            this.render();
         }
 
     };
--- root/test/app.spec.js
+++ root/test/app.spec.js
@@ -5,25 +5,76 @@ define(function(require) {
         var app = require('app'),
             _ = require('lodash');
 
+        it('should auto-initialise', function() {
+            app.initialised.should.equal(true);
+        });
+
         it('should have an #init method', function() {
-            _.isFunction(app.init).should.eql(true);
+            _.isFunction(app.init).should.equal(true);
         });
 
         it('should have an #render method', function() {
-            _.isFunction(app.render).should.eql(true);
+            _.isFunction(app.render).should.equal(true);
         });
 
         it('should have a #config property', function() {
-            _.isObject(app.config).should.eql(true);
+            _.isObject(app.config).should.equal(true);
         });
 
         it('should have a #templates property', function() {
-            _.isObject(app.templates).should.eql(true);
+            _.isObject(app.templates).should.equal(true);
         });
 
-        it('should initially render the HomeView', function() {
-            var HomeView = require('views/home');
-            app.currentView.constructor.should.eql(HomeView);
+        describe('#init', function() {
+
+            it('should attempt to set the environment based on a meta element', function() {
+                app.config.getenv().should.not.equal('prod');
+                $('meta').attr({
+                    name: 'application-env',
+                    content: 'prod'
+                }).appendTo('head');
+                app.init();
+                app.config.getenv().should.equal('prod');
+                app.config.setenv('dev');
+            });
+
+            it('should initially render the HomeView', function() {
+                var HomeView = require('views/home');
+                app.currentView.constructor.should.equal(HomeView);
+            });
+
+        });
+
+        describe('#render', function() {
+            var spy;
+
+            it('should render the currentView', function() {
+                app.currentView.should.be.ok;
+                spy = sinon.spy(app.currentView, 'render');
+                app.render();
+                spy.calledOnce.should.equal(true);
+            });
+
+        });
+
+        describe('#setTheme', function() {
+            var testTheme = 'mono';
+
+            beforeEach(function() {
+                app.setTheme('');
+            });
+
+            it('should update the config', function() {
+                app.config.get('theme').should.not.equal(testTheme);
+                app.setTheme('mono');
+                app.config.get('theme').should.equal(testTheme);
+            });
+
+            it('should update the body attribute', function() {
+                app.setTheme(testTheme);
+                $('body').attr('data-theme').should.equal(testTheme);
+            });
+
         });
 
         describe('#setView (valid)', function() {
